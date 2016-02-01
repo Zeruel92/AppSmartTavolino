506b26bcbd4794b4f913c50a6d3f8bf35e2dc8fb
pspgt <gabry.t92@gmail.com>
2016-02-01 10:47:40 +0100

    Aggiunti Commenti alle classi Add/DelPrefListener

--- app/src/main/java/iot/unisalento/it/appsmarttavolino/DelPrefListener.java
+++ app/src/main/java/iot/unisalento/it/appsmarttavolino/DelPrefListener.java
@@ -4,7 +4,11 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
 import android.view.View;
-
+/*
+ *Questa Classe fornisce il listener per i pulsanti di rimozione preferenza
+ * Quando un pulsante viene premuto viene inviata una richesta DELETE al server rest per rimuovere la specifica
+ * preferenza associata al pulsante attraverso l'id del Genere Selezionato e aggiorna l'interfaccia delle preferenze
+ */
 public class DelPrefListener implements View.OnClickListener {
     private Preferenze p;
     public DelPrefListener(Preferenze p){
@@ -16,8 +20,8 @@ public class DelPrefListener implements View.OnClickListener {
         try {
             new ClientHttp(p.getApplicationContext()).execute("DELETE", "Preferenze", Integer.toString(p.getIdUtente()), idGenere);
             SharedPreferences preferences=p.getSharedPreferences("appmuseo", Context.MODE_PRIVATE);
-            String token=preferences.getString("token", null);
-            p.updateUi(token);
+            String token=preferences.getString("token", null); //recupera il token dalla cache dello smartphone
+            p.updateUi(token); //aggiorna l'interfaccia delle preferenze
         }catch(Exception e){
             Log.e("DelPrefListener",e.getMessage());
         }
